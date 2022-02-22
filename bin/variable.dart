ca598557b38c8dad99d1de28a44153667a2df06e
riswandha <riswandhaa22@gmail.com>
2022-02-22 21:12:32 +0700

    variable

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,9 +1,25 @@
 void main() {
-  String name = 'Riswandha adhitia';
-  int age = 20;
+  final name =
+      'Riswandha adhitia'; // variable tidak bisa dideklarasikan ulang namun nilai dapat dirubah
+  final age = 20;
   var Country = 'Indonesia';
+  String job = 'President';
+  const pi =
+      3.14; //variable tidak bisa dideklarasikan ulang dan nilai tidak dapat dirubah
 
   print(name);
   print(age);
   print(Country);
+  print(job);
+  print(pi);
+
+  late var value =
+      getValue(); //variable akan diinisialisasi setelah fungsi dijalankan
+  print('Variable sudah dibuat');
+  print(value);
+}
+
+getValue() {
+  print('getValue() dipnggil');
+  return 'Riswandha Adhitia';
 }
