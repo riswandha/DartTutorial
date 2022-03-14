9a77f1f1ae50b5f367a50e574de9283bec61b861
riswandha <riswandhaa22@gmail.com>
2022-03-14 11:07:46 +0700

    number

---
+++ bin/number.dart
@@ -0,0 +1,13 @@
+void main() {
+  int number1 = 12;
+  double number2 = 12.1;
+
+  print(number1);
+  print(number2);
+
+  num number3 = 12; //num bisa integer dan double
+  print(number3);
+
+  number3 = 12.1;
+  print(number3);
+}
